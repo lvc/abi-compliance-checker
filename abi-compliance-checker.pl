e59087d8e8dd56cf19373e06a5f673c0d112b473
Andrey Ponomarenko <andrewponomarenko@yandex.ru>
2016-07-04 19:12:19 +0300

    Released 1.99.22

--- abi-compliance-checker.pl
+++ abi-compliance-checker.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl
 ###########################################################################
-# ABI Compliance Checker (ABICC) 1.99.21
+# ABI Compliance Checker (ABICC) 1.99.22
 # A tool for checking backward compatibility of a C/C++ library API
 #
 # Copyright (C) 2009-2011 Institute for System Programming, RAS
@@ -60,7 +60,7 @@ use Storable qw(dclone);
 use Data::Dumper;
 use Config;
 
-my $TOOL_VERSION = "1.99.21";
+my $TOOL_VERSION = "1.99.22";
 my $ABI_DUMP_VERSION = "3.2";
 my $XML_REPORT_VERSION = "1.2";
 my $XML_ABI_DUMP_VERSION = "1.2";
